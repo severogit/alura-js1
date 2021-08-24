ad944b4f35fdf7fea5ac3d9c41b202fe5d6ad725
Anderson Severo <severowz@gmail.com>
2021-08-24 17:18:12 -0300

    Primeiro commit, já após a finalização do curso

---
+++ README.md
@@ -0,0 +1,5 @@
+Primeiro projeto em JavaScript feito com os cursos da Alura
+
+Página usada para cadastrar pacientes com "Nome, Peso, Altura, Gordura Corporal" e usar os dados para fazer o cálculo do "IMC"
+
+Commit único, feito após o término do curso.
---
+++ js/calcula-imc.js
@@ -0,0 +1,69 @@
+// muda título da pagina
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Projeto JS";
+
+
+var pacientes = document.querySelectorAll(".paciente");
+
+
+for(var i = 0; i < pacientes.length; i++){
+
+	var paciente = pacientes[i];
+
+	var tdPeso = paciente.querySelector(".info-peso");
+	var peso = tdPeso.textContent;
+
+	var tdAltura = paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
+
+	var tdImc = paciente.querySelector(".info-imc");
+
+	var pesoValidation = validaPeso(peso); // true ou false
+	var alturaValidation = validaAltura(altura); // true ou false
+
+	if (!pesoValidation){
+		console.log("Peso Inv");
+		pesoValidation = false;
+		tdImc.textContent = "Peso inválido";
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if (!alturaValidation){
+		console.log("Altura inv");
+		alturaValidation = false;
+		tdImc.textContent = "Altura inválida";
+		paciente.classList.add("paciente-invalido");
+	}
+
+
+	if (alturaValidation & pesoValidation){
+		var imc = calculaImc(peso,altura);
+		tdImc.textContent = imc;
+	}
+
+
+}
+
+function validaPeso(peso){
+	if(peso >= 0 && peso <= 300){
+	return true;
+}else{
+	return false;
+}
+}
+
+function validaAltura(altura){
+	if(altura >= 0 && altura <= 3.00){
+		return true;
+	} else{
+		return false;
+	}
+}
+
+function calculaImc(peso,altura){
+	var imc = 0;
+
+	imc = peso / (altura * altura);
+
+	return imc.toFixed(2);
+}
